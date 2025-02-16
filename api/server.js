f6d6ff86cb40515e27810aad78e9af836224d1dc
francisco.bruno@armateus.com.br <bruno-amil@hotmail.com>
2025-02-16 12:29:14 -0300

    feat: change route url

--- api/server.js
+++ api/server.js
@@ -6,16 +6,20 @@ const PORT = 3001;
 const app = express();
 
 app.use((cors()));
-app.get('/', (req, res) => {
+app.get('/api/', (req, res) => {
     res.send('Just andpoint for artists')
 });
 
-app.get('/artists', async (req, res) => {
+app.get('/api//artists', async (req, res) => {
     res.send(await database.collection('artists').find({}).toArray())
 });
 
-app.get('/songs', async (req, res) => {
+app.get('/api/songs', async (req, res) => {
     res.send(await database.collection('songs').find({}).toArray())
+});
+
+app.get("*", async (req, res) => {
+    res.send('Acesse uma rota válida')
 });
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
